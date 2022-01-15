694c6f841abad24b42179e980c3fb5918e6a05e5
7ORP3DO <davisudha806@gmail.com>
2022-01-15 17:31:04 +0530

    add GSAP ScrollTrigger

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,24 +1,3 @@
-AOS.init();
-
-Waves.init();
-Waves.attach('button, .experience .container div', ['waves-light']);
-
-
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-async function scramble() {
-
-    let bfl = baffle('.name', {
-        characters: 'abcdefghijklmnopqrstuvwxyz',
-        speed: 60
-    });
-
-    bfl.start();
-    await sleep(4000);
-    bfl.reveal(3000);
-}
 
 async function startTyping() {
     var typed = new Typed('.typing', {
@@ -37,6 +16,94 @@ async function startTyping() {
         startDelay: 1000,
     });
 
+}
+
+AOS.init();
+
+Waves.init();
+Waves.attach('button, .experience .container div', ['waves-light']);
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function testAnimation(el) {
+    $(el).textillate({
+        loop: false,
+        in: {
+            effect: "flash"
+        }
+    });
+}
+
+function animateHeading(el) {
+    gsap.to(el, {
+        scrollTrigger: {
+            trigger: el,
+            start: "top 90%",
+            end: "bottom 50%",
+            markers: true,
+            scrub: true,
+            onEnter: () => {
+                console.log('enter')
+            },
+            toggleActions: "restart pause reverse none"
+        },
+        fontSize: '1.65rem',
+        color: '#7fff00',
+    });
+}
+
+
+function animateIcon(el) {
+    gsap.to(el, {
+        scrollTrigger: {
+            trigger: el,
+            start: "top 40%",
+            markers: true,
+            scrub: true,
+            onEnter: () => {
+                console.log('enter')
+            },
+            toggleActions: "restart pause reverse none"
+        },
+        rotate: '60deg',
+    });
+}
+
+gsap.to('section.about img', {
+    scrollTrigger: {
+        trigger: 'section.about img',
+        start: "top 10%",
+        markers: true,
+        scrub: true,
+        onEnter: () => {
+            console.log('enter')
+        },
+        toggleActions: "restart pause reverse none"
+    },
+    filter: 'blur(8px)',
+});
+
+(document.querySelectorAll(".headline")).forEach(element => {
+    animateHeading(element)
+});
+
+(document.querySelectorAll(".fab")).forEach(element => {
+    animateIcon(element)
+});
+
+
+async function scramble() {
+
+    let bfl = baffle('.name', {
+        characters: 'abcdefghijklmnopqrstuvwxyz',
+        speed: 60
+    });
+
+    bfl.start();
+    await sleep(4000);
+    bfl.reveal(3000);
 }
 
 scramble();
